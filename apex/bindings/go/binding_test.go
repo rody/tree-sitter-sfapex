abcb431a8dd8d6410e42f7d2849a5ccf0757231f
Rodolphe Blancho <rodolphe.blancho@gmail.com>
2025-05-19 23:37:15 +1000

    fix repo name

--- apex/bindings/go/binding_test.go
+++ apex/bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_apex_test
 import (
 	"testing"
 
+	tree_sitter_apex "github.com/rody/tree-sitter-sfapex/apex/bindings/go"
 	tree_sitter "github.com/tree-sitter/go-tree-sitter"
-	tree_sitter_apex "github.com/aheber/tree-sitter-sfapex.git/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
